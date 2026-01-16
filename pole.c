9a6c6fc65a1c11edcdace7c641047ce3107c003e
Filip Wajrak <Filipekww@gmail.com>
2026-01-16 13:00:21 +0100

    Dodano funkcje liczenia pola koła.

--- pole.c
+++ pole.c
@@ -1,8 +1,12 @@
 
 #include <stdio.h>
+#define PI 3.14159265
 
 float pole_prostokata(float a,float b) {
 return a*b;
+}
+float pole_kola (float r) {
+return PI*r*r;
 }
 
 void prostokat() {
@@ -19,10 +23,23 @@ void prostokat() {
 
     printf("Pole prostokata wynosi: %.2f\n", pole);
 }
+void kolo() {
+ float r, pole;
+
+    printf("Podaj promien kola: ");
+    scanf("%f", &r);
+
+
+    pole = pole_kola(r);
+
+    printf("Pole kola wynosi: %.2f\n", pole);
+}
 int main(void)
 {
 
-prostokat();
+//prostokat();
+kolo();
+
     return 0;
 
 }
